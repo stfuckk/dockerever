8607fcb31ff68f91afca8b741efb5edb55362b33
d.kozlov <d.kozlov@i-digit.ru>
2024-11-26 08:31:00 +0700

    init project

---
+++ backend/src/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER: str = os.getenv("POSTGRES_USER", "user")
+DB_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "password")
+DB_HOST: str = os.getenv("DB_HOST", "host")
+DB_PORT: str = os.getenv("DB_PORT", "port")
+DB_NAME: str = os.getenv("POSTGRES_DB", "dbname")
+
+SECRET_KEY: str = os.getenv("SECRET_KEY", "secret_key")
+ALGORITHM: str = os.getenv("ALGORITHM", "algorithm")

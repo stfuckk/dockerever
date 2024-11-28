f6ac17797c86977c53dacfbea331ccf8f36ace5f
d.kozlov <d.kozlov2205@mail.ru>
2024-11-29 02:22:47 +0700

    init sveltekit+flowbite frontend

---
+++ frontend/src/lib/global/NavigationBar.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { page } from '$app/stores'
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+    import { DarkMode } from 'flowbite-svelte';
+    $: activeUrl = $page.url.pathname;
+</script>
+  
+
+<Navbar rounded color="form" class="fixed z-20 border-b">
+    <NavBrand href="/">
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">DockerEver</span>
+    </NavBrand>
+    <NavHamburger />
+    <div class="flex items-center">
+        <NavUl {activeUrl} class="flex space-x-4">
+            <NavLi href="/" active={true}>Home</NavLi>
+            <NavLi href="/about">About</NavLi>
+        </NavUl>
+        <DarkMode class="mk-4"/>
+    </div>
+</Navbar>
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import '../app.css';
+    import NavigationBar from '$lib/global/NavigationBar.svelte';
+
+    let { children } = $props();
+</script>
+
+{@render children()}
+
+<NavigationBar></NavigationBar>
